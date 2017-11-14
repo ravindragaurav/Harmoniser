ef144674f2f811eab598d70fc321851fc58c7953
Gaurav Ravindra <ravindra_gaurav@yahoo.com>
2017-11-14 19:27:23 +0000

    Clean up done. Removed all default stuff and test project

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -43,7 +43,7 @@ namespace Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Harmoniser}/{id?}");
             });
         }
     }
